fa084bc083da0bbce4d2994a17ca1b08b109c9b9
aristotelis <aristotelis79@gmail.com>
2021-05-19 12:08:10 +0300

    add responsivness

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,15 +1,24 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
+const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  isScreenSmall: boolean;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakpointObserver
+    //.observe([Breakpoints.XSmall ]) //deafults
+    .observe([`( max-width: ${SMALL_WIDTH_BREAKPOINT}px `])
+    .subscribe((state: BreakpointState) => {
+      this.isScreenSmall = state.matches;
+    });
   }
 
 }
