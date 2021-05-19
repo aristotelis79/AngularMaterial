826c0a112fd980003eeaa95529de977196db2f71
aristotelis <aristotelis79@gmail.com>
2021-05-19 14:01:13 +0300

    add icon and usr sanitazier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   { path: 'contactmanager', loadChildren: () => import('./contactmanager/contantmanager.module').then(m => m.ContantmanagerModule)},
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
