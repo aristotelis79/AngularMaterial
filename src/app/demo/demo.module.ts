04c9a0e4be300f040def24de0caa410ff506389d
aristotelis <aristotelis79@gmail.com>
2021-05-19 10:34:39 +0300

    add flex layout styles

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -6,6 +6,7 @@ import { ButtonsComponent } from './buttons/buttons.component';
 import { MaterialModule } from '../shared/material.module';
 import { FormsModule } from '@angular/forms';
 import { FlexboxComponent } from './flexbox/flexbox.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { FlexboxComponent } from './flexbox/flexbox.component';
   imports: [
     CommonModule,
     DemoRoutingModule,
+    FlexLayoutModule,
     MaterialModule,
     FormsModule
   ]
