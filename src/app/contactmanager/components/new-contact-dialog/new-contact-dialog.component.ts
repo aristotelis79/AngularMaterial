eb5159e1ec966681dab2d483d2499de216584423
aristotelis <aristotelis79@gmail.com>
2021-05-20 08:53:36 +0300

    add new contact

---
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-new-contact-dialog',
+  templateUrl: './new-contact-dialog.component.html',
+  styleUrls: ['./new-contact-dialog.component.scss']
+})
+export class NewContactDialogComponent implements OnInit {
+
+  user: User;
+  avatars = ['svg-1', 'svg-2', 'svg-3', 'svg-4',]
+
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
+
+  ngOnInit(): void {
+    this.user = new User();
+  }
+
+  save() {
+    this.dialogRef.close(this.user);
+  }
+
+  dismiss() {
+    this.dialogRef.close(null);
+  }
+
+}
--- src/app/contactmanager/contantmanager.module.ts
+++ src/app/contactmanager/contantmanager.module.ts
@@ -12,6 +12,7 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NotesComponent } from './components/notes/notes.component';
+import { NewContactDialogComponent } from './components/new-contact-dialog/new-contact-dialog.component';
 
 const routes: Routes = [
   { path: '', component: ContactmanagerAppComponent,
@@ -29,7 +30,8 @@ const routes: Routes = [
     ToolbarComponent,
     MainContentComponent,
     SidenavComponent,
-    NotesComponent
+    NotesComponent,
+    NewContactDialogComponent
   ],
   imports: [
     CommonModule,
