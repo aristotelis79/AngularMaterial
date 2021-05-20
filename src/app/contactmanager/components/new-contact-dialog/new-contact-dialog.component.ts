16b08d1c1726e06599172bd3d5bc13dc967cdfaa
aristotelis <aristotelis79@gmail.com>
2021-05-20 20:35:35 +0300

    Add datepiker and validator

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from '../../models/user';
 
@@ -10,7 +11,12 @@ import { User } from '../../models/user';
 export class NewContactDialogComponent implements OnInit {
 
   user: User;
-  avatars = ['svg-1', 'svg-2', 'svg-3', 'svg-4',]
+  avatars = ['svg-1', 'svg-2', 'svg-3', 'svg-4']
+  name = new FormControl('', [Validators.required]);
+
+  getErrorMessage() {
+    return this.name.hasError('required') ? 'You must enter a name' : '';
+  }
 
   constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
 
@@ -19,6 +25,7 @@ export class NewContactDialogComponent implements OnInit {
   }
 
   save() {
+    this.user.name = this.name.value;
     this.dialogRef.close(this.user);
   }
 
--- src/app/contactmanager/contantmanager.module.ts
+++ src/app/contactmanager/contantmanager.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { MaterialModule } from '../shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
@@ -39,6 +39,7 @@ const routes: Routes = [
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [
